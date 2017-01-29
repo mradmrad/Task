2fe3db3e551580a5b885387928a01ed5f9f5ce47
MRAD <haithemmrad22@gmail.com>
2017-01-29 13:30:45 +0100

    new file

--- Sportifs/app/AppKernel.php
+++ Sportifs/app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FrontBundle\FrontBundle(),
+            new BackBundle\BackBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
